6aa07ba8fc28547315bbee52454858fd86d761a0
Hal Stroemeria <fripfrop@docomo.ne.jp>
2018-02-15 20:00:46 +0900

    コンシューマーキーも削除

--- Plugin/Opauth/config.php
+++ Plugin/Opauth/config.php
@@ -2,18 +2,18 @@
 <?php
 // 各プロバイダ毎のAPIキー
 $config = [
-    'security_salt' => '2sBz1U5epdrtub6DQH26MfzhXAehaf1H55SRwTBu', // レスポンスのシグネチャ生成に使うソルト値(ランダム文字列)
-    'path' => '/auth/',
-    'callback_url' => '/auth/complete', // Opauthのソーシャルログイン処理完了後にリダイレクトするURL
+	'security_salt' => '2sBz1U5epdrtub6DQH26MfzhXAehaf1H55SRwTBu',
+	'path' => '/auth/',
+    'callback_url' => '/auth/complete', 
     'Strategy' => [
         'Facebook' => [
-            'app_id' => '1611552788953011',
+            'app_id' => '',//アプリキー設定
             'app_secret' => '', //シークレットを設定
             'scope' => 'email',
             'fields' => 'email,first_name,last_name,name'
         ],
         'Twitter' => [
-            'key' => 'zOKTxAKJk4AhGjVRwrz2IAuCb',
+            'key' => '', //コンシューマーキー設定
 			'secret' =>'' //シークレットを設定
         ]
     ]
