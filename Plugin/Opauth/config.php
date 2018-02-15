019dd874a4ef77b1a780c00d23f79a1a65fc9914
Hal Stroemeria <fripfrop@docomo.ne.jp>
2018-02-15 19:48:52 +0900

    シークレットキーを記述しないように変更

--- Plugin/Opauth/config.php
+++ Plugin/Opauth/config.php
@@ -6,23 +6,15 @@ $config = [
     'path' => '/auth/',
     'callback_url' => '/auth/complete', // Opauthのソーシャルログイン処理完了後にリダイレクトするURL
     'Strategy' => [
-        'Yahoojp' => [
-            'client_id' => 'xxx',
-            'client_secret' => 'xxx'
-        ],
         'Facebook' => [
             'app_id' => '1611552788953011',
-            'app_secret' => 'c269a9661f08729e653c4732134e75e7',
+            'app_secret' => '', //シークレットを設定
             'scope' => 'email',
             'fields' => 'email,first_name,last_name,name'
         ],
-        'Google' => [
-            'client_id' => 'xxx',
-            'client_secret' => 'xxx'
-        ],
         'Twitter' => [
             'key' => 'zOKTxAKJk4AhGjVRwrz2IAuCb',
-            'secret' => 'vC1myTCqzCqSYFIUnc08Q2hlO2ulupwYrOmF0NlML33ncPLrr7'
+			'secret' =>'' //シークレットを設定
         ]
     ]
 ];
